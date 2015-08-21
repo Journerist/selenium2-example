36133d31b966e8b3bb67ee8d1544e43a1de9861a
Sebastian <akrillo89@gmail.com>
2015-08-21 21:39:39 +0200

    simple selenium 2 testng test

---
+++ test/e2e/AureliaSkeletionNavigationTest.java
@@ -0,0 +1,54 @@
+package e2e;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.net.URL;
+
+
+public class AureliaSkeletionNavigationTest {
+    private WebDriver driver;
+
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setBrowserName("firefox");
+
+        DesiredCapabilities capabilitiesChrome = DesiredCapabilities.chrome();
+        capabilitiesChrome.setBrowserName("chrome");
+
+        driver = new RemoteWebDriver(
+                new URL("http://localhost:4444/wd/hub"),capabilities);
+
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void testSimple() throws Exception {
+        driver.get("http://192.168.117.1:9000");
+
+        waitUntilLoadingDivDisappears();
+
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Aurelia");
+    }
+
+    private void waitUntilLoadingDivDisappears() {
+        wait.until(
+                ExpectedConditions.not(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("splash"))));
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
